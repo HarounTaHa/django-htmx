640f25cfd14a4836a6188012bff6bd867eaf9788
HarounTaHa <harountaha@outlook.sa>
2025-03-25 13:06:56 +0200

    create search functionality with htmx

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from contacts.models import Contact
+from django.db.models import Q
 
 
 @login_required
@@ -8,3 +9,11 @@ def index(request):
     contacts = request.user.contacts.all().order_by('-created_at')
     context = {'contacts': contacts}
     return render(request, 'contacts.html', context)
+
+
+@login_required
+def search_contact(request):
+    query = request.GET.get('search', '')
+    # use the query to filter contacts by name or email
+    contacts = request.user.contacts.filter(Q(name__icontains=query) | Q(email__icontains=query))
+    return render(request, 'partials/contact-list.html', {'contacts': contacts})
