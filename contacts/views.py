9dfeb100de47d7200b716942e6f1119b080d828b
HarounTaHa <harountaha@outlook.sa>
2025-03-25 12:27:09 +0200

    retrieve contacts

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from contacts.models import Contact
 
-# Create your views here.
+
+@login_required
 def index(request):
-    context = {}
+    contacts = request.user.contacts.all().order_by('-created_at')
+    context = {'contacts': contacts}
     return render(request, 'contacts.html', context)
